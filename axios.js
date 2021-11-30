0739e1259bd458feeb11ef0536c65202303b9491
MrAzuka <johntwice8@gmail.com>
2021-11-30 11:30:06 +0100

    feat: Add new routes

--- axios.js
+++ axios.js
@@ -3,11 +3,84 @@ const request = require('request');
 const app = express();
 const cheerio = require('cheerio');
 
-let title, company, link;
-const jobs = [];
+
 
 app.get('/', (req, res) => {
+    let title, company, link;
+    const jobs = [];
     const url = 'https://www.jobberman.com/jobs?sort=featured';
+
+    request(url, function (err, response, html) {
+        const $ = cheerio.load(html);
+        let c = 0;
+        let l = 0;
+        $('.break-words').each(function () {
+            const data = $(this);
+
+            title = data.attr('title');
+            jobs.push({ title });
+        });
+
+        $('.search-result__job-meta').each(function () {
+            const data = $(this);
+
+            company = data.text().trim();
+            jobs[c].company = company;
+            c++;
+        });
+
+        $('.break-words').each(function () {
+            const data = $(this);
+
+            link = data.attr('href');
+            jobs[l].link = link;
+            l++;
+        });
+
+        console.log(jobs);
+    });
+});
+
+app.get('/1', (req, res) => {
+    let title, company, link;
+    const jobs = [];
+    const url = 'https://www.jobberman.com/jobs';
+
+    request(url, function (err, response, html) {
+        const $ = cheerio.load(html);
+        let c = 0;
+        let l = 0;
+        $('.break-words').each(function () {
+            const data = $(this);
+
+            title = data.attr('title');
+            jobs.push({ title });
+        });
+
+        $('.search-result__job-meta').each(function () {
+            const data = $(this);
+
+            company = data.text().trim();
+            jobs[c].company = company;
+            c++;
+        });
+
+        $('.break-words').each(function () {
+            const data = $(this);
+
+            link = data.attr('href');
+            jobs[l].link = link;
+            l++;
+        });
+
+        console.log(jobs);
+    });
+});
+
+app.get('/2', (req, res) => {
+    let title, company, link;
+    const jobs = [];
+    const url = 'https://www.jobberman.com/jobs/remote';
 
     request(url, function (err, response, html) {
         const $ = cheerio.load(html);
